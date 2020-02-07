3966d986b3648b3a4092dc1399e9bd326872fbbf
filipton12 <37213766+filipton12@users.noreply.github.com>
2020-02-07 23:54:21 +0100

    make 16 Reverse String & 28 Fibonacci Numbers

---
+++ 1.4e/16-Reverse-A-String/16-Reverse-A-String/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Reverse_A_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put text you want to reverse:");
+
+            Console.WriteLine(ReverseString(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        public static string ReverseString(string texttoreverse)
+        {
+            char[] stringchars = new char[texttoreverse.Length];
+
+            //get all chars from string
+            StringReader sr = new StringReader(texttoreverse);
+            sr.Read(stringchars, 0, texttoreverse.Length);
+
+            //convert array to list 
+            List<char> textchars = stringchars.ToList();
+
+            //reverse list
+            textchars.Reverse();
+
+            //build string
+            StringBuilder reversedString = new StringBuilder();
+
+            for(int i = 0; i < textchars.Count; i++)
+            {
+                reversedString.Append(textchars[i]);
+            }
+
+            return reversedString.ToString();
+        }
+    }
+}
---
+++ 1.4e/28-Fibonacci-Sequence/28-Fibonacci-Sequence/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Threading;
+
+namespace _28_Fibonacci_Sequence
+{
+    class Program
+    {
+        public static bool StopBool = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select option:");
+            Console.WriteLine("1) Until Stop");
+            Console.WriteLine("2) Input index, get output.");
+            Console.WriteLine("3) Exit");
+
+
+            start:
+            switch (Console.ReadLine().ToLower())
+            {
+                case "1":
+                    Thread threadUntilStop = new Thread(PrintFibonacciNumbers);
+                    threadUntilStop.Start();
+                    break;
+                case "2":
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.WriteLine(GetFibonacciNumberAtIndex((BigInteger.Parse(Console.ReadLine())) + 1));
+                        }
+                        catch { }
+                    }
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    goto start;
+            }
+
+            Console.ReadLine();
+        }
+
+        public static BigInteger GetFibonacciNumberAtIndex(BigInteger index)
+        {
+            BigInteger a = 0;
+            BigInteger b = 1;
+
+            BigInteger c = 0;
+
+            for(BigInteger i = 2; i < index; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
+        }
+
+        public static void PrintFibonacciNumbers()
+        {
+            BigInteger Times = 0;
+
+            BigInteger a = 0;
+            BigInteger b = 1;
+
+            BigInteger c = 0;
+
+            while (StopBool == true)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                Console.WriteLine(c);
+                Thread.Sleep(0);
+                Times++;
+                Console.Title = Times.ToString();
+            }
+        }
+    }
+}
